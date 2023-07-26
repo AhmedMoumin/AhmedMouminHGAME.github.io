f88fcf75c5b48278d8c4057f8bd1990b8fe37454
AhmedMoumin <140558614+AhmedMoumin@users.noreply.github.com>
2023-07-26 14:50:34 +0800

    Add files via upload

---
+++ Bad Game/Javascript/EN/java.js
@@ -0,0 +1,11 @@
+function err() {
+    if (document.getElementById('input').value == "NAME") {
+        document.getElementById("err").innerHTML = " ERROR ! THE NAME CAN'T BE 'NAME' "
+    }
+}
+function frenchbutton() {
+    window.location.href = '/HTML(Whith Inner CSS)/FR/fr.html' 
+}
+function engbutton() {
+    window.location.href = '/HTML(Whith Inner CSS)/EN/index.html'
+}
